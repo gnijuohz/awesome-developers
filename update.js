2d65f90fbf42355315055b65395c3016e537820e
Jing Zhou <gnijuohz@gmail.com>
2018-01-20 13:21:16 -0800

    added twitter handles for some people

--- update.js
+++ update.js
@@ -1,10 +1,14 @@
-const github = require('octonode');
 const fs = require('fs');
+const github = require('octonode');
+const Twit = require('twit');
+
+const people = require('./people.json').people;
+const twitterCredentials = require('./twitter-keys');
 
 const client = github.client();
 const ghsearch = client.search();
 
-const people = require('./people.json').people;
+const T = new Twit(twitterCredentials);
 
 const peopleLookup = {};
 for (let person of people) {
@@ -25,9 +29,7 @@ function searchUser(options) {
   });
 }
 
-/**
- * update people.json
- */
+// update the list via github api
 function updatePeople() {
   const a = [...Array(10).keys()];
   Promise.all(
@@ -56,18 +58,62 @@ function updatePeople() {
           tech: found.keywords || [],
         };
       });
-      fs.writeFileSync(
-        'people.json',
-        JSON.stringify({ people: users }, null, 2),
-        'utf-8',
-        (err) => {
-          if (err) throw err;
-        }
-      );
+      writeToJSON(users);
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
-updatePeople();
+// really simple heuristic that might be wrong... but a lot
+// of people are using the same handle for github and twitter
+// another one that's not used here is firstname_lastname
+function isPotentialMatch(twitterInfo, person) {
+  let url;
+  let entities = twitterInfo.entities;
+  if (entities && entities.url) {
+    url = entities.url.urls[0].expanded_url;
+    if (url && url.includes(`github.com/${person.github}`)) {
+      return true;
+    }
+    if (url === person.blog) return true;
+  }
+  // if they don't have that many followers, either this is not
+  // the right one or they are not active, either way can be ignored
+  if (twitterInfo.followers_count > 500) {
+    return true;
+  }
+  return false;
+}
+
+
+async function updateTwitter() {
+  for (let person of people) {
+    if (!person.twitter) {
+      const info = await T.get('users/show', {
+        screen_name: person.github,
+      });
+      data = info.data;
+      if (isPotentialMatch(data, person)) {
+        person.twitter = person.github;
+      }
+    }
+  }
+  return people;
+}
+
+function writeToJSON(people) {
+  fs.writeFileSync(
+    'people.json',
+    JSON.stringify({ people: people }, null, 2),
+    'utf-8',
+    (err) => {
+      if (err) throw err;
+    }
+  );
+}
+
+// updateGithub();
+updateTwitter().then((people) => {
+  writeToJSON(people);
+});
